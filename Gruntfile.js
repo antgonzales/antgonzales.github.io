f4f2c345f967bbf06a1039219a5825dbe5450949
Anthony Gonzales <anthony@thirdandgrove.com>
2014-10-01 22:30:26 -0400

    Update Grunt packages and Grunt file.

--- Gruntfile.js
+++ Gruntfile.js
@@ -132,16 +132,6 @@ module.exports = function(grunt){
   		}
   });
 
-  grunt.loadNpmTasks("grunt-sass");
-  grunt.loadNpmTasks("grunt-contrib-cssmin");
-  grunt.loadNpmTasks("grunt-contrib-uglify");
-  grunt.loadNpmTasks("grunt-contrib-jshint");
-  grunt.loadNpmTasks("grunt-contrib-concat");
-  grunt.loadNpmTasks('grunt-contrib-imagemin');
-  grunt.loadNpmTasks("grunt-contrib-watch");
-  grunt.loadNpmTasks("grunt-shell");
-  
-
   grunt.registerTask("default",  ["jekyll"]);
   grunt.registerTask("buildcss",  ["sass", "cssc", "cssmin"]);
   grunt.registerTask("buildjs",  ["concat", "uglify"]);
