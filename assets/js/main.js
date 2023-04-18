71e72b145dddf073c02a51305ab85c46800fa48f
Anthony Gonzales <3740655+antgonzales@users.noreply.github.com>
2023-04-18 15:49:51 -0400

    changed header, added modernizr and html5shiv, created a contact form

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,103 +1,156 @@
+// Avoid `console` errors in browsers that lack a console.
+(function() {
+  var method;
+  var noop = function () {};
+  var methods = [
+    'assert', 'clear', 'count', 'debug', 'dir', 'dirxml', 'error',
+    'exception', 'group', 'groupCollapsed', 'groupEnd', 'info', 'log',
+    'markTimeline', 'profile', 'profileEnd', 'table', 'time', 'timeEnd',
+    'timeStamp', 'trace', 'warn'
+  ];
+  var length = methods.length;
+  var console = (window.console = window.console || {});
 
-$(function() {
-  // Smoothe scroll 
-  // Credit: Chris Coyier
-  // Find it at http://css-tricks.com/snippets/jquery/smooth-scrolling/  
-  $('a[href*=#]:not([href=#])').click(function() {
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+  while (length--) {
+    method = methods[length];
+
+    // Only stub undefined methods.
+    if (!console[method]) {
+        console[method] = noop;
+    }
+  }
+}());
+
+$(function () {
+
+// Intro loading screen
+  $(window).load(function() {
+    // Animate loader off screen
+    $('.loading-spinner').fadeOut(800);
+    $('.loading-screen').fadeOut(800);
+  });
+
+// Smoothe scroll 
+  $('a[href*=#]:not([href=#])').click(function () {
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
       var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-      if (target.length) {
-        $('html,body').animate({
-          scrollTop: target.offset().top
-        }, 800);
-        return false;
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+    if (target.length) {
+      $('html,body').animate({
+      scrollTop: target.offset().top
+      }, 800);
+      return false;
       }
     }
   });
 
-  var width = $(window).width();
+  $("#hero-title").fitText(1.2, { minFontSize: '40px', maxFontSize: '108px'});
+  $("#hero-tagline").fitText(2.5, { minFontSize: '21px', maxFontSize: '54px'});
 
-  if (width >= 1200) {
-    $('[data-typer-targets]').typer();
-    $.typer.options.highlightSpeed = 100;
-    $.typer.options.typeSpeed = 200;
-  }
+// Chart data and
+  var skillsChartData = [{
+    value: 25,
+    color: "#424651"
+  }, {
+    value: 25,
+    color: "#85BAB0"
+  }, {
+    value: 20,
+    color: "#F4D569"
+  }, {
+    value: 10,
+    color: "#ED1941"
+  }, {
+    value: 10,
+    color: "#575151"
+  }, {
+    value: 10,
+    color: "#92CDED"
+  }];
 
-  // Chart data and chart transition
-  var skillsChartData = [
-    {
-        value: 25,
-        color:"#424651"
-    },
-    {
-        value : 25,
-        color : "#85BAB0"
-    },
-    {
-        value : 20,
-        color : "#F4D569"
-    },    
-    {
-        value : 10,
-        color : "#ED1941"
-    },     
-    {
-        value : 10,
-        color : "#575151"
-    },
-    {
-        value : 10,
-        color : "#92CDED"
-    }        
-  ];
-
-  function showSkillsChart(){   
+  function showSkillsChart() {
     var ctx = document.getElementById("skills-chart").getContext("2d");
     new Chart(ctx).Pie(skillsChartData);
-  };
+  }
 
-  // Waypoint transitions
+// Waypoint transitions
   var graphInitDelay = 300;
 
-    $('#recent-work').waypoint(function(direction) {
-      $("#aat-screenshot").removeClass("invisible");
-      $("#aat-screenshot").addClass("animated fadeInLeft");
-    }, {
-      triggerOnce: true,
-      offset: "60%"
-    });
+  $('#recent-work').waypoint(function (direction) {
+    $("#aat-screenshot").removeClass("invisible");
+    $("#aat-screenshot").addClass("animated fadeInLeft");
+  }, {
+    triggerOnce: true,
+    offset: "60%"
+  });
 
-    $('#skills').waypoint(function(direction) {
-      $("#skills-chart-container").removeClass("invisible");
-      $(".chart-legend").addClass("animated fadeInUp");
-      setTimeout(showSkillsChart,graphInitDelay);
-    }, {
-      triggerOnce: true,
-      offset: "60%"
-    });
-    
+  $('#skills').waypoint(function (direction) {
+    $("#skills-chart-container").removeClass("invisible");
+    $(".chart-legend").addClass("animated fadeInUp");
+  setTimeout(showSkillsChart, graphInitDelay);
+  }, {
+    triggerOnce: true,
+    offset: "60%"
+  });
 
-  // Sticky Nav
-  var stickyNav = function(){
-    var top = $("#home").height();  
-    var scrollTop = $(document).scrollTop();  
-         
-    if (scrollTop >= top) {   
-        $("#sticky-nav").addClass("sticky");
-        $("#sticky-nav").removeClass("hidden");
-        $("#sticky-nav").removeClass("animated slideOutUp");
-        $("#sticky-nav").addClass("animated slideInDown");
-    } else if (scrollTop >= 0 )  {
-        $("#sticky-nav").removeClass("animated slideInDown");
-        $("#sticky-nav").addClass("animated slideOutUp");
+
+// Sticky Nav
+  var stickyNav = function () {
+    width = $(window).width();
+    var top = $(".hero").height();
+    var scrollTop = $(window).scrollTop();
+
+    if (scrollTop >= top && width >= 577) {
+      $("#sticky-nav").addClass("sticky");
+      $("#sticky-nav").slideDown("fast");
+    } else if (scrollTop >= 0) {
+      $("#sticky-nav").slideUp("fast");
     }
-  };  
-      
-    stickyNav();  
-      
-    $(window).scroll(function() {  
-        stickyNav();  
-    });  
+  };
+
+  stickyNav();
+
+  $(window).scroll(function () {
+    stickyNav();
+  });
+
+// Form Validation and Simple Form Submission
+  $(".ajax-form").validate({
+    rules: {
+      name: {
+        required: true,
+        minlength: 2,
+      },
+      email: {
+        required: true,
+        email: true,
+      },
+      message: {
+        required: true,
+        minlength: 5,
+    },
+  },
+  errorPlacement: function(error, element) {
+  },
+  submitHandler: function(form) {
+    $.ajax({
+      dataType: "jsonp",
+      url: "http://getsimpleform.com/messages/ajax?form_api_token=f3424adaa86654f85e3c0931cbffc00f",
+      data: $(".ajax-form").serialize() 
+    }).done(function() {
+      //callback which can be used to show a thank you message
+      //and reset the form
+      $(".ajax-form").hide();
+      $(".form-thank-you").fadeIn("400");
+    });
+      return false; //to stop the form from submitting
+    }
+  });
 
 });
+
+
+
+
+
+
